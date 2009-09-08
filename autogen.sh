f481744f7f6db9c024b2898a06344208edb17be0
Jacob Kroon <jacob.kroon@gmail.com>
2009-09-08 19:20:49 +0200

    GNUify

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,5 @@
 libtoolize --automake            || exit 1
 aclocal -Ibuild                  || exit 1
-automake --foreign --add-missing || exit 1
+automake --add-missing           || exit 1
 autoconf                         || exit 1
 ./configure $*
